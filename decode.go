87638501185c1cad38f4713f801d4fcb7e4f4a69
Nages <197055139@qq.com>
2023-05-19 16:51:14 +0800

    .

--- decode.go
+++ decode.go
@@ -6,18 +6,10 @@ import (
 
 // Decode parses SNBT data to the provided value of a pointer s.
 func Decode(r []byte, s interface{}) error {
-	//re := regexp.MustCompile(`^#.+|[ \n\t\r]#.*[^"][\n]+|#$`)
-	////re := regexp.MustCompile(`^#.*\n`)
-	//r = re.ReplaceAll(r, []byte(""))
 	lxr := newLexer(r)
 	if err := lxr.tokenize(); err != nil {
 		return err
 	}
-	//for _, tk := range lxr.tks {
-	//	fmt.Println("------------------------------------------")
-	//	fmt.Println(tk.t, tk.v)
-	//	fmt.Println("------------------------------------------")
-	//}
 	psr := newParser(lxr.tokens())
 	dmap, err := psr.parse()
 	if err != nil && err != ErrExhaustedAllTokens {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module mc-snbt
 
 go 1.20
+
+require github.com/mitchellh/mapstructure v1.5.0
